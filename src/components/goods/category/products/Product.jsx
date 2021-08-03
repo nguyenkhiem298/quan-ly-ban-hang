8311bf6c6fb4b0b2d38c43313a3948f88d2d14bf
khiem_nguyen <nguyenkhiem01649526298@gmail.com>
2021-08-03 10:07:13 +0700

    change style display

--- src/components/goods/category/products/Product.jsx
+++ src/components/goods/category/products/Product.jsx
@@ -1,6 +1,21 @@
 import React, { Component } from 'react'
 
 export default class Product extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            displayProductDetail: 'none'
+        }
+    }
+
+    handleClick = () => {
+        if(this.state.displayProductDetail === 'none') {
+            this.setState({displayProductDetail: 'table-row'})
+        }else {
+            this.setState({displayProductDetail: 'none'})
+        }
+    }
+
     render() {
         return (
             <>
@@ -12,6 +27,24 @@ export default class Product extends Component {
                     <td>{this.props.pro.price}</td>
                     <td>{this.props.pro.inventory}</td>
                     <td><button onClick={ this.handleClick } type="button" className="btn btn-secondary">Action</button></td>
+                </tr>
+                <tr style={{display: this.state.displayProductDetail}} id={this.props.pro.id}>
+                    <td colSpan="7">
+                        <ul className="nav nav-tabs">
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">Thông Tin</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Thẻ Kho</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Tồn Kho</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Liên Kết Kênh Bán Hàng</a>
+                            </li>
+                        </ul>
+                    </td>
                 </tr>
             </>
         )
